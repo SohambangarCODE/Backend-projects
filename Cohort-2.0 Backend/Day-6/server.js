5332bfb9c963c85922c685c00fd0621c3a63d654
Soham Bangar <sohambangar188@gmail.com>
2026-01-29 14:28:47 +0530

    successfully connected to database

--- Cohort-2.0 Backend/Day-6/server.js
+++ Cohort-2.0 Backend/Day-6/server.js
@@ -1,7 +1,19 @@
 //start server
 //connect database
-const app = require('./src/app')
+const app = require("./src/app");
+const mongoose = require("mongoose");
 
-app.listen(3000, ()=>{
-    console.log("server is running on port 300!")
-})
+function Connectedtodb() {
+  mongoose
+    .connect(
+      "mongodb+srv://Soham:GQLnE2YUgj2ElGAW@cluster0.4mqjhvm.mongodb.net/Day-6",
+    )
+    .then(() => {
+      console.log("Connected to Database");
+    });
+}
+Connectedtodb()
+
+app.listen(3000, () => {
+  console.log("server is running on port 300!");
+});
