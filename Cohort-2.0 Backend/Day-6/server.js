4de96fcaedec343e767673ddafa1abead1869eba
Soham Bangar <sohambangar188@gmail.com>
2026-01-29 12:52:33 +0530

    server created and going to connect with database

---
+++ Cohort-2.0 Backend/Day-6/server.js
@@ -0,0 +1,7 @@
+//start server
+//connect database
+const app = require('./src/app')
+
+app.listen(3000, ()=>{
+    console.log("server is running on port 300!")
+})
---
+++ Cohort-2.0 Backend/Day-6/src/app.js
@@ -0,0 +1,15 @@
+//server create
+//server config
+
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+
+
+
+
+
+
+
+module.exports = app
